8f7d820131f46dcf937b30c6784d094a4058e3c3
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-05-15 23:54:28 +0300

    add formula

---
+++ Formula/certonid.rb
@@ -0,0 +1,42 @@
+class Certonid < Formula
+  CERTONID_VERSION = '0.9.1'
+
+  desc "Certonid"
+  homepage "https://github.com/certonid/certonid"
+  version CERTONID_VERSION
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/certonid/certonid/releases/download/#{CERTONID_VERSION}/certonid_#{CERTONID_VERSION}_Darwin_arm64.tar.gz"
+    sha256 "7fd750303531a2fb76d658f68591f612090339a8ddabc7fb7388581c8d93e73a"
+  end
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/certonid/certonid/releases/download/#{CERTONID_VERSION}/certonid_#{CERTONID_VERSION}_Darwin_x86_64.tar.gz"
+    sha256 "e6457766d580dd319d58343a644c63ef110972426dec6ad293eeb8cce680c792"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/certonid/certonid/releases/download/#{CERTONID_VERSION}/certonid_#{CERTONID_VERSION}_Linux_x86_64.tar.gz"
+    sha256 "ea2cf529d40177f689a074390a5b822e738ea6370da0b7c2fb7e7784d5ca77fa"
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://github.com/certonid/certonid/releases/download/#{CERTONID_VERSION}/certonid_#{CERTONID_VERSION}_Linux_arm.tar.gz"
+    sha256 "fe5c067f56640abf4cd822d8ae30b32136fb6f999c6ff89f515c6436de924f34"
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/certonid/certonid/releases/download/#{CERTONID_VERSION}/certonid_#{CERTONID_VERSION}_Linux_arm64.tar.gz"
+    sha256 "3f67cb72802d159cd70a26f9899444d78658caac5232bed9e729e89062d041ad"
+  end
+
+  conflicts_with "certonid"
+
+  def install
+    bin.install "certonid"
+  end
+
+  test do
+    system "#{bin}/certonid --version"
+  end
+end
--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-# Certonid Tap
+# Certonid Homebrew Tap
 
-## How do I install these formulae?
+## What is Homebrew?
 
-`brew install certonid/tap/<formula>`
+Package manager for macOS (or Linux), see more at https://brew.sh
 
-Or `brew tap certonid/tap` and then `brew install <formula>`.
+## What is a Tap?
+
+A third-party (in relation to Homebrew) repository providing installable
+packages (formulae) on macOS and Linux.
+
+See more at https://docs.brew.sh/Taps
+
+## How do I install certonid cli from here?
+
+```sh
+brew install certonid/tap/certonid
+```
+
+You can also add the tap which makes formulae within it
+available in search results (`brew search` output):
+
+```sh
+brew tap certonid/tap
+brew install certonid
+```
 
 ## Documentation
 
